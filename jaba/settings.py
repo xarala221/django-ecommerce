03fcdcde4d30f9d6b89080b910d6965d603d66db
xaralaxarala@gmail.com <xaralaxarala@gmail.com>
2022-04-23 02:18:32 +0000

    Add static

--- jaba/settings.py
+++ jaba/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "jaba" / "static"
 
 STATICFILES_DIR = [
     BASE_DIR / "static",
