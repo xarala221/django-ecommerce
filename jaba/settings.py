8818a2eafd495c25cc13691197c94d58e03ead2b
xaralaxarala@gmail.com <xaralaxarala@gmail.com>
2022-04-23 01:58:32 +0000

    update database

--- jaba/settings.py
+++ jaba/settings.py
@@ -79,17 +79,6 @@ WSGI_APPLICATION = 'jaba.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jaba_local',
-        'USER': 'postgres',
-        'PASSWORD': 'abc123!',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
